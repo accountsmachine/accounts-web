bf654a462a09c123b3a2c07e66c5b470e48b019a
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-03-27 13:51:32 +0100

    Show transaction using mat-table

--- src/app/commerce/transaction/transaction.component.ts
+++ src/app/commerce/transaction/transaction.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { MatTableDataSource } from '@angular/material/table';
+
 import { Transaction } from '../commerce.model';
 import { CommerceService } from '../commerce.service';
+
+type Row = {
+    label : string,
+    value : string,
+};
 
 @Component({
     selector: 'app-transaction',
@@ -15,6 +22,11 @@ export class TransactionComponent implements OnInit {
     id = "";
     tx : any = {};
 
+    data : MatTableDataSource<Row> =
+	new MatTableDataSource<Row>([]);
+
+    columns = ["label", "value"];
+
     constructor(
 	private route : ActivatedRoute,
 	private commerce : CommerceService,
@@ -24,7 +36,7 @@ export class TransactionComponent implements OnInit {
 	    this.txs = txs;
 	    console.log(this.txs);
 	    if (this.txs[this.id]) {
-		this.tx = this.txs[this.id];
+		this.load(this.txs[this.id]);
 	    }
 	});
 	this.route.params.subscribe(
@@ -32,8 +44,7 @@ export class TransactionComponent implements OnInit {
 		if (params["id"]) {
 		    this.id = params["id"];
 		    if (this.txs[this.id]) {
-			this.tx = this.txs[this.id];
-			console.log(this.tx);
+			this.load(this.txs[this.id]);
 		    }
 		}
 	    }
@@ -44,4 +55,92 @@ export class TransactionComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    load(tx : Transaction) {
+	let data : Row[] = [];
+
+	data.push({ "label": "ID", "value": this.id });
+	data.push({ "label": "Time", "value": tx.time });
+
+	if (tx.filing)
+	    data.push({ "label": "Filing", "value": tx.filing });
+
+	if (tx.company)
+	    data.push({ "label": "Company", "value": tx.company });
+
+	data.push({ "label": "Transaction", "value": tx.transaction });
+
+	if (tx.name)
+	    data.push({ "label": "Name", "value": tx.name });
+
+	if (tx.address)
+	    data.push({ "label": "Address", "value": tx.address.join(", ") });
+
+	if (tx.postcode)
+	    data.push({ "label": "Postcode", "value": tx.postcode });
+
+	if (tx.country)
+	    data.push({ "label": "Country", "value": tx.country });
+
+	if (tx.billing_country)
+	    data.push({ "label": "Billing country", "value": tx.billing_country });
+
+	if (tx.uid)
+	    data.push({ "label": "UID", "value": tx.uid });
+
+	if (tx.email)
+	    data.push({ "label": "Email", "value": tx.email });
+
+	if (tx.order) {
+	    for (let item of tx.order.items) {
+
+		let amount = "";
+		if (item.amount) {
+		    amount = (item.amount / 100).toFixed(2).toString();
+		    amount = " (£" + amount +")";
+		}
+
+		let rsrc;
+		if (item.kind == "vat") rsrc = "VAT credit";
+		if (item.kind == "corptax") rsrc = "Corp tax credit";
+		if (item.kind == "accounts") rsrc = "Accounts filing";
+
+		if (rsrc) {
+		    data.push({
+			"label": rsrc, "value": item.quantity.toString() + amount,
+		    });
+		}
+
+	    }
+
+	    if (tx.order.subtotal)
+		data.push({
+		    "label": "Subtotal",
+		    "value": "£" + (tx.order.subtotal / 100).toFixed(2).toString()
+		});
+
+	    if (tx.order.vat)
+		data.push({
+		    "label": "VAT",
+		    "value": "£" + (tx.order.vat / 100).toFixed(2).toString()
+		});
+
+	    if (tx.order.total)
+		data.push({
+		    "label": "Total",
+		    "value": "£" + (tx.order.total / 100).toFixed(2).toString()
+		});
+
+	}
+
+	if (tx.valid != undefined)
+	    data.push({ "label": "Valid", "value": tx.valid.toString() });
+
+	if (tx.vat_number)
+	    data.push({ "label": "VAT no.", "value": tx.vat_number });
+
+	this.data.data = data;
+
+    }
+
 }
+
