a97cd93a156453a7948ccd8fc8f270f3306640dc
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-03-28 17:14:14 +0100

    Stripe in a separate service

--- src/app/commerce/checkout/checkout.component.ts
+++ src/app/commerce/checkout/checkout.component.ts
@@ -23,103 +23,35 @@ export class CheckoutComponent implements OnInit {
     @ViewChild(StripePaymentElementComponent)
     paymentElement? : StripePaymentElementComponent = undefined;
 
-    elementsOptions: StripeElementsOptions = {
-	locale: 'en',
-
-	// Seems to be ignored.
-	appearance: {
-	    theme: 'stripe'
-	}
-
-    };
-
     constructor(
 	private service : CheckoutService,
 	private stripeService : StripeService,
 	private router : Router,
-	private payments : StripePaymentService,
+	public payments : StripePaymentService,
     ) { }
 
     payment_intent : PaymentIntent | null = null;
 
     ngOnInit(): void {
-
 	this.service.create_order().subscribe(txid => {
 	    this.service.create_payment(txid).subscribe(pid => {
-		this.elementsOptions.clientSecret = pid;
+		this.payments.set_payment_id(pid);
 	    });
 	});
-	    
-//	    this.elementsOptions.clientSecret = pi.client_secret;
-//	});
-
-//	this.payments.
-
-	/*
-	this.create_payment_intent().subscribe(pi => {
-	    if (pi && pi.client_secret) {
-		this.elementsOptions.clientSecret = pi.client_secret;
-		this.payment_intent = pi;
-	    } else {
-		this.payment_intent = null;
-	    }
-	});
-*/
     }
 
     place_order() {
-	this.stripeService.confirmPayment({
-	    elements: this.paymentElement!.elements,
-	    confirmParams: {
-		payment_method_data: {
-		    billing_details: {
-			name: "accountsmachine.io",
-		    }
-		}
-	    },
-	    redirect: 'if_required',
-	}).subscribe(result => {
-	    console.log(result);
-
-	    if (result && result.paymentIntent) {
-
-		let status = result.paymentIntent.status;
-		console.log("Status>", status);
-
-		let id = result.paymentIntent.id;
-
-		if (status != "succeeded") {
-		    console.log("FIXME: Payment failed");
-		    return;
-		}
-
-		if (id) {
-		    console.log("ID>", id);
-		    this.service.complete_payment(id).subscribe(res => {
-			console.log("Complete order is done.");
-			console.log(res);
-		    });
-		    return;
-		}
-		
-		console.log("Client secret is NULL?!!");
-
-	    }
+	this.payments.confirm(this.paymentElement).subscribe(pid => {
+	    this.service.complete_payment(pid).subscribe(res => {
+		console.log("PAYMENT COMPLETE");
+		console.log(res);
+	    });
 	});
-
     }
 
     back() {
 	this.router.navigate(["/commerce/shop"]);
     }
-/*
-    create_payment_intent() : Observable<PaymentIntent> {
-	return new Observable<PaymentIntent>((obs : any) => {
-	    this.service.create_order().subscribe(ev => {
-		obs.next(ev);
-	    })
-	});
-	}*/
 
     create_payment() : Observable<PaymentIntent> {
 	return new Observable<PaymentIntent>((obs : any) => {
@@ -131,3 +63,4 @@ export class CheckoutComponent implements OnInit {
 
 
 }
+
--- src/app/commerce/stripe.service.ts
+++ src/app/commerce/stripe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { StripeService, StripePaymentElementComponent
        } from 'ngx-stripe';
@@ -8,11 +9,73 @@ import { StripeElementsOptions, PaymentIntent
 import { CheckoutService } from './checkout.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'any'
 })
 export class StripePaymentService {
 
-  constructor() { }
+    elementsOptions: StripeElementsOptions = {
+	locale: 'en',
+
+	// Seems to be ignored.
+	appearance: {
+	    theme: 'stripe'
+	}
+
+    };
+
+    constructor(
+	private stripeService : StripeService,
+    )
+    {
+    }
+
+    confirm(element : any) {
+	return new Observable<string>(obs => {
+	    this.stripeService.confirmPayment({
+		elements: element!.elements,
+		confirmParams: {
+		    payment_method_data: {
+			billing_details: {
+			    name: "accountsmachine.io",
+			}
+		    }
+		},
+		redirect: 'if_required',
+	    }).subscribe(result => {
+		console.log(result);
+		if (result && result.paymentIntent) {
+		    let status = result.paymentIntent.status;
+		    console.log("Status>", status);
+		    
+		    let id = result.paymentIntent.id;
+		    
+		    if (status != "succeeded") {
+			console.log("FIXME: Payment failed");
+			return;
+		    }
+
+		    if (id) {
+			console.log("ID>", id);
+			obs.next(id);
+			console.log("Complete order!");
+			return;
+		    }
+		
+		    console.log("Client secret is NULL?!!");
+
+		}
+	    });
+	});
+
+    }
+
+    get_payment_id() {
+	return this.elementsOptions.clientSecret;
+    }
+
+    set_payment_id(id : string) {
+	this.elementsOptions.clientSecret = id;
+    }
 
 }
 
