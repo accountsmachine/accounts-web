4862f7917c543318660d58569ec849fa0bab8f60
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-04-28 19:20:57 +0100

    Stage should use auth key for prod.

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -5,7 +5,7 @@ export const environment = {
     firebase: {
         // Stage uses the prod backends and auth, but stage-hosted
 	// app and api.
-	apiKey: "AIzaSyBD9stQwa1BX3CIN1YjEktA",
+	apiKey: "AIzaSyBD9stQwa1BX3CIN1YjEktA-bRWHqM3FfA",
 	authDomain: "accounts-machine-prod.firebaseapp.com",
 	projectId: "accounts-machine-prod",
     },
