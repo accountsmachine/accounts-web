52eb27a6591b7092e90a44092a3649ca8e3182c6
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-05-31 13:55:18 +0100

    Added commented-out crypto feature tag to stage/prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,6 +14,7 @@ export const environment = {
 //	"vat-submit",
 //	"corptax-submit",
 //	"accounts-submit",
+//	"crypto",
     ],
 };
 
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -22,6 +22,7 @@ export const environment = {
 	"vat-submit",
 	"corptax-submit",
 	"accounts-submit",
+//	"crypto",
     ],
 };
 
