59e727920d90b00af3b5548662e3c7d8e6a594d1
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-06-22 10:29:24 +0100

    Switch stage to prod auth

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -3,16 +3,12 @@ export const environment = {
     production: true,
     useEmulators: false,
     firebase: {
+
         // Stage uses the prod backends and auth, but stage-hosted
 	// app and api.
-//	apiKey: "AIzaSyBD9stQwa1BX3CIN1YjEktA-bRWHqM3FfA",
-//	authDomain: "accounts-machine-prod.firebaseapp.com",
-//	projectId: "accounts-machine-prod",
-
-	apiKey: "AIzaSyA1m3XnFvLIZu7CWKHOjGv1N3o3pmRFtEY",
-	authDomain: "accounts-machine-dev.firebaseapp.com",
-	projectId: "accounts-machine-dev",
-
+	apiKey: "AIzaSyBD9stQwa1BX3CIN1YjEktA-bRWHqM3FfA",
+	authDomain: "accounts-machine-prod.firebaseapp.com",
+	projectId: "accounts-machine-prod",
 
     },
     features: [
